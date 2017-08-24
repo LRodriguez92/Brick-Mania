d2570e07ae6295860325db70c44ce635e3e94a4c
Leonardo Rodriguez <leo@Leonardos-Air.fios-router.home>
2017-08-24 15:35:57 -0400

    Created autoplay button for play testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public bool autoPlay;
+	
+	private Ball ball; 
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +14,11 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		PaddleControl();
+		if (autoPlay) {
+			AutoPlay();
+		} else {
+			PaddleControl();		
+		}
 	}
 	
 	// The controls for the paddle
@@ -21,4 +28,14 @@ public class Paddle : MonoBehaviour {
 		paddle.x = Mathf.Clamp (mousePos, 1f, 15f);
 		this.transform.position = paddle;
 	}
+	
+	// Paddle automatically moves with ball
+	void AutoPlay () {
+		ball = FindObjectOfType<Ball>();
+		float ballPos = ball.transform.position.x;
+		Vector2 paddlePos = new Vector2 (ballPos, this.transform.position.y);
+		paddlePos.x = Mathf.Clamp(ballPos, 1f, 15f);
+		this.transform.position = paddlePos;
+	}
+	
 }
