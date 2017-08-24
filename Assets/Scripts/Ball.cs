46de9c217c7e2aeb8a8479efd80c1a91816d84b2
Leonardo Rodriguez <leo@Leonardos-Air.fios-router.home>
2017-08-24 14:56:51 -0400

    tweaked ball velocity

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -21,10 +21,17 @@ public class Ball : MonoBehaviour {
 		 	//Launch Ball
 			if (Input.GetMouseButtonDown(0)) {
 				hasStarted = true;
-				this.rigidbody2D.velocity = new Vector2 (3f, 18f);
+				this.rigidbody2D.velocity = new Vector2 (3f, 16f);
 			}
 		}	
 	}
 	
+	void OnCollisionEnter2D (Collision2D coll) {
+		Vector2 tweak = new Vector2 (.2f, .2f);
+		
+		if (hasStarted) {
+			rigidbody2D.velocity += tweak;
+		}
+	}
 	
 }
