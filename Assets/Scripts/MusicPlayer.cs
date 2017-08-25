901b37927da51780f5033c8b39b972e30272f35c
Leonardo Rodriguez <leo@Leonardos-Air.home>
2017-08-25 10:07:59 -0400

    music player added

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static bool musicPlayer = false;
+
+	// Use this for initialization
+	void Awake () {
+		if (musicPlayer == false) {
+			audio.Play();
+			Debug.Log ("Music start");
+			musicPlayer = true;
+			DontDestroyOnLoad(gameObject);
+		} else if (musicPlayer) {
+			Destroy(gameObject);
+			Debug.Log ("Deleted duplicate player");
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
